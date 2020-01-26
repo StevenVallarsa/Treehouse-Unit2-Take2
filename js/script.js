ac3009a11707d62059dbf4f0498179bd86d1a3df
StevenVallarsa <orionsjv@gmail.com>
2020-01-25 21:14:20 -0500

    Completed basic project

--- js/script.js
+++ js/script.js
@@ -5,17 +5,6 @@ FSJS project 2 - List Filter and Pagination
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 /* * *
  * Global Variables
  * * */
@@ -23,20 +12,10 @@ FSJS project 2 - List Filter and Pagination
 const list = document.querySelectorAll(".student-item");
 const itemPerPage = 10;
 let pageNumber = 1; // starting page number
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+/* * *
+ * Function to set display property to show or no show on each list item
+ * * */
 
 const showPage = (list, page) => {
   let startIndex = page * itemPerPage - itemPerPage;
@@ -51,18 +30,18 @@ const showPage = (list, page) => {
   }
 };
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+/* * *
+ * Function to add pagination to bottom of list
+ * Includes click handler function to make non-active page buttons clickable
+ * * */
 
-const appendPageLinks = list => {
+const appendPageLinks = () => {
   const page = document.querySelector(".page");
+
   const numberOfPages = Math.ceil(list.length / itemPerPage);
 
   const div = document.createElement("div");
   div.setAttribute("class", "pagination");
-
   const ul = document.createElement("ul");
 
   for (let i = 1; i <= numberOfPages; i++) {
@@ -75,30 +54,27 @@ const appendPageLinks = list => {
     } else {
       a.className = "active";
     }
-
     li.appendChild(a);
     ul.appendChild(li);
   }
 
   div.appendChild(ul);
   page.appendChild(div);
+  let pageClick = document.querySelector(".pagination");
+
+  pageClick.addEventListener("click", e => {
+    if (e.target.tagName === "A") {
+      pageNumber = parseInt(e.target.innerHTML);
+      showPage(list, pageNumber);
+
+      const deletePagination = document.querySelector(".page");
+      const ulChild = deletePagination.lastChild;
+      deletePagination.removeChild(ulChild);
+      appendPageLinks();
+    }
+  });
 };
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-// for ()
-
+//runs script for the first time. Other iterations will be done through the appendPageLink function
 showPage(list, pageNumber);
-appendPageLinks(list);
-
-const pageClick = document.querySelector(".pagination");
-pageClick.addEventListener("click", e => {
-  if (e.target.tagName === "A") {
-    pageNumber = parseInt(e.target.innerHTML);
-    showPage(list, pageNumber);
-
-    const deletePagination = document.querySelector(".pagination");
-    const ulChild = deletePagination.firstChild;
-    deletePagination.removeChild(ulChild);
-    appendPageLinks(list);
-  }
-});
+appendPageLinks();
