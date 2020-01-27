2503ff5ee71ea5aa0d798a142c95626beccc9365
StevenVallarsa <orionsjv@gmail.com>
2020-01-27 17:35:08 -0500

    Finished Project - Exceeds Expectations

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ let pageNumber = 1; // starting page number
  * * */
 
 const showPage = (list, page) => {
-  let startIndex = page * itemsPerPage - itemsPerPage;
-  let endIndex = page * itemsPerPage;
+  const startIndex = page * itemsPerPage - itemsPerPage;
+  const endIndex = page * itemsPerPage;
 
   for (let i = 0; i < list.length; i++) {
     if (i + 1 > startIndex && i < endIndex) {
@@ -63,7 +63,7 @@ const appendPageLinks = list => {
   div.appendChild(ul);
   page.appendChild(div);
 
-  let pageClick = document.querySelector(".pagination");
+  const pageClick = document.querySelector(".pagination");
 
   pageClick.addEventListener("click", e => {
     if (e.target.tagName === "A") {
@@ -82,63 +82,70 @@ function deletePaginationLinks() {
   deletePagination.removeChild(ulChild);
 }
 
-// create search / reset buttons
-const searchFunction = () => {};
-const search = document.querySelector(".page-header");
+/* * *
+ * Create search / reset buttons
+ * * */
 
-const searchDiv = document.createElement("div");
-searchDiv.className = "student-search";
-const searchInput = document.createElement("input");
-searchInput.type = "text";
-searchInput.placeholder = "Search for students...";
-const searchButton = document.createElement("button");
-searchButton.textContent = "Search";
-searchButton.className = "submit";
-const resetButton = document.createElement("button");
-resetButton.textContent = "Reset";
-resetButton.className = "reset";
-searchDiv.appendChild(searchInput);
-searchDiv.appendChild(searchButton);
-searchDiv.appendChild(resetButton);
-search.appendChild(searchDiv);
+const searchFunction = () => {
+  const search = document.querySelector(".page-header");
 
-const searchButtonClick = document.querySelector(".submit");
-const resetButtonClick = document.querySelector(".reset");
-const h3 = document.querySelectorAll("h3");
+  const searchDiv = document.createElement("div");
+  searchDiv.className = "student-search";
+  const searchInput = document.createElement("input");
+  searchInput.type = "text";
+  searchInput.placeholder = "Search for students...";
+  const searchButton = document.createElement("button");
+  searchButton.textContent = "Search";
+  searchButton.className = "submit";
+  const resetButton = document.createElement("button");
+  resetButton.textContent = "Reset";
+  resetButton.className = "reset";
+  searchDiv.appendChild(searchInput);
+  searchDiv.appendChild(searchButton);
+  searchDiv.appendChild(resetButton);
+  search.appendChild(searchDiv);
 
-// search button listener
-searchButtonClick.addEventListener("click", e => {
-  e.preventDefault();
-  let newList = [];
-  let searchTerm = searchInput.value;
-  for (let i = 0; i < list.length; i++) {
-    if (
-      searchInput.value.length !== 0 &&
-      h3[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())
-    ) {
-      list[i].style.display = "";
-      newList.push(list[i]);
-    } else {
-      list[i].style.display = "none";
+  const searchButtonClick = document.querySelector(".submit");
+  const resetButtonClick = document.querySelector(".reset");
+  const h3 = document.querySelectorAll("h3");
+
+  // search button listener
+  searchButtonClick.addEventListener("click", e => {
+    e.preventDefault();
+    const newList = [];
+    let searchTerm = searchInput.value;
+    for (let i = 0; i < list.length; i++) {
+      if (
+        searchInput.value.length !== 0 &&
+        h3[i].textContent
+          .toLowerCase()
+          .includes(searchInput.value.toLowerCase())
+      ) {
+        list[i].style.display = "";
+        newList.push(list[i]);
+      } else {
+        list[i].style.display = "none";
+      }
     }
-  }
 
-  searchInput.value = "";
-  pageNumber = 1;
-  showPage(newList, 1);
-  deletePaginationLinks();
-  appendPageLinks(newList);
-});
+    // reset button listener
+    resetButtonClick.addEventListener("click", () => {
+      pageNumber = 1;
+      showPage(list, 1);
+      deletePaginationLinks();
+      appendPageLinks(list);
+    });
 
-// reset button listener
-resetButtonClick.addEventListener("click", () => {
-  pageNumber = 1;
-  showPage(list, 1);
-  deletePaginationLinks();
-  appendPageLinks(list);
-});
+    searchInput.value = "";
+    pageNumber = 1;
+    showPage(newList, 1);
+    deletePaginationLinks();
+    appendPageLinks(newList);
+  });
+};
 
-// Runs script on load.
+// Runs scripts on load.
 // Other iterations will be done through the appendPageLink function.
 showPage(list, pageNumber);
+searchFunction();
 appendPageLinks(list);
